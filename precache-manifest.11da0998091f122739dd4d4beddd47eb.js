4f79a54619ae0098e49de2b15560751aad92deda
RuiFan Xu <ruifanxu@ruifans-mbp.home>
2019-11-19 18:32:20 -0800

    Updates

---
+++ precache-manifest.11da0998091f122739dd4d4beddd47eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb62cf8f719559182b5386c118aed035",
+    "url": "/inf551_project/index.html"
+  },
+  {
+    "revision": "3161147ab610dbe37f4f",
+    "url": "/inf551_project/static/css/2.fb9ed26a.chunk.css"
+  },
+  {
+    "revision": "e9de4c25ada4f3b11f44",
+    "url": "/inf551_project/static/css/main.07b8c5de.chunk.css"
+  },
+  {
+    "revision": "3161147ab610dbe37f4f",
+    "url": "/inf551_project/static/js/2.c8c7b98d.chunk.js"
+  },
+  {
+    "revision": "e9de4c25ada4f3b11f44",
+    "url": "/inf551_project/static/js/main.2a329b35.chunk.js"
+  },
+  {
+    "revision": "9f21d1135dc5745b49ee",
+    "url": "/inf551_project/static/js/runtime-main.cb0423df.js"
+  }
+]);
